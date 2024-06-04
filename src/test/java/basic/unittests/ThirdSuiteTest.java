3978e5d58007162603a05ba7d65c5874529a903d
uitesterhero <150478429+uitesterhero@users.noreply.github.com>
2024-06-03 20:16:07 -0500

    Update ThirdSuiteTest.java

--- src/test/java/basic/unittests/ThirdSuiteTest.java
+++ src/test/java/basic/unittests/ThirdSuiteTest.java
@@ -1,8 +1,11 @@
 package basic.unittests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.fail;
 
 public class ThirdSuiteTest {
+    @Ignore
     @Test
     public void thirdTest1() {
         System.out.println("thirdTest1 successful!");
@@ -10,11 +13,11 @@ public class ThirdSuiteTest {
 
     @Test
     public void thirdTest2() throws Exception {
-        System.out.println("thirdTest2 successful!");
+        fail("forcing a test failure");
     }
 
     @Test
-    public void thirdTest3() {
-        System.out.println("thirdTest3 successful!");
+    public void thirdTest3() throws Exception {
+        throw new Exception("Trying to throw an error");
     }
 }
